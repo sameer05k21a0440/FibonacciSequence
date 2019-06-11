19448f1ba0437964f141e6fb896b707de3495e44
sameer05k21a0440 <sameersoft6@gmail.com>
2019-06-11 15:20:01 +0800

    Updated JS file Push Method added

--- fibonacci.component.js
+++ fibonacci.component.js
@@ -2,16 +2,16 @@ function getFibonacciSeq(){
    count=5;
    n1=0,n2=1;
    n3=null;
+   var digits = [];
    var  inkk= document.getElementById('input-id-val').value;
-    console.log(n1)
-    console.log(n2)
-    document.write(n1)
-    document.write(n2)
+    digits.push(n1)
+    digits.push(n2)
     for(i=2;i<count;i++){
       n3=n1+n2;
       n1=n2;    
       n2=n3;    
-      console.log(n3);
-      document.write(n3)
+      digits.push(n3)
     }
+    console.log(digits);
+    document.write(digits)
     }
